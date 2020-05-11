aa6ea9feb16673d7bbd9c46e95849cef1a60d5a3
Hallie Dunn <hallie.dunn1@gmail.com>
2020-05-11 14:17:28 -0700

    hangout json now carrying more useful info for frontend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,31 @@ class UsersController < ApplicationController
 
         # check if we have a user by email or username
         if !!@user && @user.valid?
-            render json: { user: @user, introverts: @user.introverts, purposes: Purpose.all}, status: 200
+            hangs = find_and_format_hangouts(@user)
+            puts "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
+            puts hangs
+            puts "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
+            render json: { user: @user, introverts: @user.introverts, purposes: Purpose.all, hangouts: hangs}, status: 200
         else
             render json: { error: 'no user found'}, status: :not_acceptable
         end
+        # refactor to use current_user
+        # why isn't current_user working
+    end
+
+    def find_and_format_hangouts(user)
+        hangs = []
+        user.friendships.each do |ship|
+            # byebug
+            # make the data readable for the front end
+            ship.hangouts.each do |hang|
+                hangs.push({hang.purpose.title=> hang.friendship.introvert.name})
+                # byebug
+            end
+            # byebug
+            # hangs.push({ship.hangouts => ship.introvert})
+        end
+        return hangs.flatten
     end
 
     private
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :name, :introverts
+  attributes :id, :username, :email, :name, :introverts, :hangouts
 end
