6827e19086fc5110832b132a2a9157530d8ba184
Hallie Dunn <hallie.dunn1@gmail.com>
2020-05-11 11:16:48 -0700

    can't hangout with introverts on cooldown

--- app/controllers/hangouts_controller.rb
+++ app/controllers/hangouts_controller.rb
@@ -6,20 +6,24 @@ class HangoutsController < ApplicationController
     end
 
     def create
+        # find user, check if introvert is on cooldown
         usr = current_user
         int = usr.introverts.find_by({name: params[:introvert]})
-        ship = usr.friendships.find_by({introvert_id: int.id})
-        purpose = Purpose.all.find_by({title: params[:purpose]})
-        # puts params
-        # find the friendship
-        # something like user.friendships.select
-        # byebug
-        hang = Hangout.new({friendship_id: ship.id, purpose_id: purpose.id, location: 'unknown', duration:60})
-        # also need to add a location and duration i guess
-        if hang.save
-            render json: {message: "Hangout Saved!"}
+        if int.on_cooldown 
+            render json: {error: "You can't hangout with that introvert right now! Try back when their cooldown is over."}
         else
-            render json: {error: "Internal error: please try again!"}
+            ship = usr.friendships.find_by({introvert_id: int.id})
+            purpose = Purpose.all.find_by({title: params[:purpose]})
+            # set the introvert on cooldown
+            int.on_cooldown = true
+            int.save
+            hang = Hangout.new({friendship_id: ship.id, purpose_id: purpose.id, location: 'unknown', duration:60})
+            # also need to add a location and duration from frontend info -- but that's details
+            if hang.save
+                render json: {message: "Hangout Saved!"}
+            else
+                render json: {error: "Internal error: please try again!"}
+            end
         end
     end
     
