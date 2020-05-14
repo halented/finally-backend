e91879d29eacd86fbc27e5f28524c96b53e86445
Hallie Dunn <hallie.dunn1@gmail.com>
2020-05-14 14:27:21 -0700

    now pulling hangout per month data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,10 +32,11 @@ class UsersController < ApplicationController
         hangs = []
         user.friendships.each do |ship|
             # make the data readable for the front end -- return the purpose of the hangout, the friend, and the date
-            # later on look into this: created_at.strftime("%Y-%m-%d") -- it will return something like "2020-01-10" to the frontend instead. will probably make that cleaner
+            # later on look into this: refactor how this info is being sent to use created_at.month, day, year. this way we don't have to save all the months in the frontend
             ship.hangouts.each do |hang|
 
                 hangs.push({hang.purpose.title=> [hang.friendship.introvert.name, hang.created_at, hang.id]})
+                
             end
         end
         return hangs
@@ -55,14 +56,42 @@ class UsersController < ApplicationController
     end
 
     def chart
-        # puts "HIT THE ROUTE CHART CHART ROUTE HIT THE ROUTE CHART CHART ROUTE HIT THE ROUTE CHART CHART ROUTE HIT THE ROUTE CHART CHART ROUTE HIT THE ROUTE CHART CHART ROUTE HIT THE ROUTE CHART CHART ROUTE HIT THE ROUTE CHART CHART ROUTE"
-        # byebug
-        data =  [
-            {x: 1, y: 4}, 
-            {x: 2, y: 7},
-            {x: 3, y: 6}
-        ]
+        # below is the format we need to send back for the victoryline
+        # data =  [
+        #     {x: 1, y: 4}, 
+        #     {x: 2, y: 7},
+        #     {x: 3, y: 6}
+        # ]
+        data = crunchChartData
+        
         render json: {data: data}
+    end
+
+    def crunchChartData
+        # what we need to look at is hangouts per mo. so really i have to go into each friendship, and in each
+        user = User.find_by(id: params[:id])
+        data = []
+        allHangouts = user.friendships.map{|ship|ship.hangouts}.flatten!
+
+        allHangouts.each do |hang| 
+            mo = hang.created_at.month
+            # check if any of the objects inside data have a key of x with a value that matches mo
+            did_it = false
+            if data.length > 0
+                data.each do |obj|
+                    if obj['x'] == mo
+                        obj['y'] += 1
+                        did_it = true
+                    end
+                end
+                if did_it == false
+                    data.push({'x'=> mo, 'y'=>1})
+                end
+            else
+                data.push({'x'=> mo, 'y'=>1})
+            end
+        end
+        return data
     end
 
     private
