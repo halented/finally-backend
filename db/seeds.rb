d1dabd6dfd90bb8a6bfde211ffcc8904ba9f7abb
Hallie Dunn <hallie.dunn1@gmail.com>
2020-05-18 17:27:43 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ img_array = ["bear", "bull", "bun", "dog", "flam", "koala", "fallback"]
 test1 = User.create(username: "tobuskin", password: "a1b2c3d4e5", email: "h.tobusk@ymail.com", name: "Holland Tobuskin")
 test2 = User.create(username: "runples", password: "a1b2c3d4e56766", email: "ishlan@youginsaavfamily.com", name: "Ishlan Youginsaav")
 test3 = User.create(username: "varicelli", password: "22222", email: "greenpreen67@yahoo.com", name: "Evan Preen Varicelli")
+test4 = User.create(username: "tiinymonster", password: "111", email: "tiiny@rocketmail.com", name: "Bernadette Tini")
 
 
 10.times do 
